8725eaed66100d52c7a7c64672f2c7892cba66c5
simon <simon.bailly5@gmail.com>
2019-05-11 23:14:30 +0200

    [Feat] DELETE + PUT

--- main.py
+++ main.py
@@ -1,54 +1,96 @@
 # coding: utf-8
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from src.entities.entity import Session, engine, Base
 from src.entities.map import Map, MapSchema
+from datetime import datetime
 
 app = Flask(__name__)
 
-# CORS policy - ok for development, then check if I want to be more restrictive
+# cross origin policy - ok for development, then check if I want to be more restrictive
 CORS(app)
 
 # if needed create database schema
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 
 
 @app.route('/')
 def index():
-    return jsonify('Hello world')
+    return 'Welcome to my API'
 
 
-@app.route('/maps')
-def get_maps():
+@app.route('/maps/list')
+def get():
     # fetch from database
     session = Session()
     maps_object = session.query(Map).all()
 
     # serialize output
     schema = MapSchema(many=True)
-    maps = schema.dump(maps_object)
+    resp = schema.dump(maps_object)
 
     session.close()
-    return jsonify(maps.data), 200
+    return jsonify(resp.data), 200
 
 
-@app.route('/maps', methods=['POST'])
-def add_maps():
+@app.route('/maps/add', methods=['POST'])
+def post():
     # deserialize input
     json_data = request.get_json()
-    input = MapSchema().load(json_data)
+    map_tdb = MapSchema().load(json_data)
 
     # create new map
-    map = Map(title=input.data['title'], description=input.data['description'], created_by="HTTP postrequest")
+    map_object = Map(title=map_tdb.data['title'], description=map_tdb.data['description'], created_by="Simon")
 
-    # persist to database
+    # add & persist to database
     session = Session()
-    session.add(map)
+    session.add(map_object)
     session.commit()
-    session.close()
 
-    return jsonify('Congrats ! You\'ve just created a new map!'), 201
+    # send it as request response - serialized
+    schema = MapSchema(many=False)
+    resp = schema.dump(map_object)
+
+    session.close()
+    return jsonify(resp.data), 201
+
+
+@app.route('/maps/update/<int:id>', methods=['PUT'])
+def put(id):
+    # deserialize input
+    json_data = request.get_json()
+    map_tdb = MapSchema().load(json_data)
+
+    # get map to update
+    session = Session()
+    map_object = session.query(Map).get(id)
+
+    # update & persist to database
+    map_object.description = map_tdb.data['description']
+    map_object.title = map_tdb.data['title']
+    map_object.updated_at = datetime.now()
+    session.commit()
+
+    # send it as request response - serialized
+    schema = MapSchema(many=False)
+    resp = schema.dump(map_object)
+
+    session.close()
+    return jsonify(resp.data), 200
+
+
+@app.route('/maps/delete/<int:id>', methods=['DELETE'])
+def delete(id):
+    # get map to delete
+    session = Session()
+    map_object = session.query(Map).get(id)
+
+    # delete & persist to database
+    session.delete(map_object)
+    session.commit()
+
+    session.close()
+    return jsonify('You\'ve just deleted a map successfully'), 200
 
 
 if __name__ == "__main__":
